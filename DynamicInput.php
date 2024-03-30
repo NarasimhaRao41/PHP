e21feed19379164f52faaf0b8ef7ce913f26f919
NarasimhaRao41 <143867308+NarasimhaRao41@users.noreply.github.com>
2024-03-30 09:42:50 +0530

    DynamicInput.php

---
+++ DynamicInput.php
@@ -0,0 +1,9 @@
+<?php
+$a = (int)readline('Enter the value of A : ');
+$b = (int)readline('Enter the value of B : ');
+echo "The sum is  ". $a+$b ."\n";
+echo "The difference is  ".$a-$b."\n";
+echo "The product is  ".$a*$b."\n";
+echo "The quotient is  ".$a/$b."\n";
+echo "The remainder is  ".$a%$b;
+?>
